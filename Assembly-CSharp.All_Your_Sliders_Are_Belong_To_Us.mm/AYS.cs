9b72907908f2a0d926f38525bfb25809f75e1f12
MassiveBattlebotsFan <teamlightning2018@outlook.com>
2023-11-01 12:12:03 -0500

    Add project files.

---
+++ Assembly-CSharp.All_Your_Sliders_Are_Belong_To_Us.mm/AYS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using MonoMod;
+using Mono.Cecil;
+
+public class patch_GameSettings : GameSettings
+{
+    private extern void orig_Start();
+    private void Start()
+    {
+        if(this.damageSliders != null)
+        {
+            foreach(var i in this.damageSliders)
+            {
+                Debug.Log(i.name + ".maxValue: " + i.maxValue.ToString());
+                i.maxValue *= 100;
+                InputField tmpInpt = i.transform.parent.GetComponentsInChildren<InputField>()[0];
+                if (tmpInpt != null) {
+                    Debug.Log("Text object val: " + tmpInpt.text);
+                    tmpInpt.textComponent.fontSize = 18;
+                    tmpInpt.characterLimit = 6;
+                }
+            }
+        }
+        orig_Start();
+    }
+}
